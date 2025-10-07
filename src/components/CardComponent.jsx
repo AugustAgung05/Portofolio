5f3c62fc9b8b1e894b7a02464f5f4af80d480b67
Ananda Agung Styadi <bapaksehat65@gmail.com>
2025-10-07 18:14:21 +0700

    fix card clas

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -4,8 +4,8 @@ import "../styles/style.css"
 function CardComponent({ className, title, description, descriptionClass }) {
   return (
     <div className={`bg-bg p-2 rounded-sm tracking-wider ${className}`}>
-      <h3 className={`text-secondary font-light text-sm md:text-base xl:text-lg`}>{title}</h3>
-      <h1 className={`font-extrabold text-neon mt-1 text-lg sm:text-2xl xl:text-3xl ${descriptionClass}`}>{description}</h1>
+      <h3 className={`text-secondary font-light text-xs sm:text-sm xl:text-base 2xl:text-lg`}>{title}</h3>
+      <h1 className={`font-extrabold text-neon mt-1 text-lg sm:text-xl xl:text-2xl ${descriptionClass}`}>{description}</h1>
     </div>
   );
 }
